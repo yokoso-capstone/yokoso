8b58e229d9eebea2d918a9d043d6598e179145ab
Tommy Xu <tommy.xu@cengn.ca>
2021-03-04 11:12:08 -0500

    header component, requires addition of logo to left side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,11 @@
 import { ReactElement } from "react";
-import { Stack, FormControl, InputGroup, InputLeftAddon, Input, Icon, Flex, Text } from "@chakra-ui/react";
+import { Button } from "@/components/core/Button";
 
 function HomePage(): ReactElement {
   return (
-    <Flex justify='center' align='center' paddingTop='30px'>
-      <Stack spacing={3} isInline>
-        <Stack spacing={3}>
-          <Text>Location:</Text>
-          <FormControl isRequired>
-            <InputGroup>
-              <InputLeftAddon children={<Icon name='info' />} />
-              <Input type='name' placeholder='City' aria-lable='City'/>
-            </InputGroup>
-          </FormControl>
-        </Stack>
-        <Stack spacing={3}>
-          <Text>Renters:</Text>
-          <FormControl isRequired>
-            <InputGroup>
-              <Input type='number' placeholder='1' aria-lable='Number of Renters'/>
-            </InputGroup>
-          </FormControl>
-        </Stack>
-      </Stack>
-    </Flex>
+    <>
+      <Button>hi</Button>
+    </>
   );
 }
 
