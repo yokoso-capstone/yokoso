a82db4476096ef05ce5588b5024f4897983c787a
Vivian Diec <viviandiec@gmail.com>
2021-03-10 13:09:52 -0500

    add button and search input components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import { ReactElement } from "react";
 import { Button } from "@/components/core/Button";
+import { BlackButton, WhiteButton, TransparentButton, RedButton } from "@/components/core/Button";
+import SeachInput from "@/components/core/SearchInput";
 
 function HomePage(): ReactElement {
   return (
     <>
       <Button>hi</Button>
+      <BlackButton>Get in Touch</BlackButton>
+      <WhiteButton>Get in Touch</WhiteButton>
+      <TransparentButton>Get in Touch</TransparentButton>
+      <RedButton>Get in Touch</RedButton>
+      <SeachInput />
     </>
   );
 }
