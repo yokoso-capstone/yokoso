09c4e9249eabf7729842e8ae5d3b7dea204945eb
Vivian Diec <viviandiec@gmail.com>
2021-03-29 22:22:44 -0400

    add header for dashboard

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -19,73 +19,83 @@ function Header(props: HeaderProps): ReactElement {
   const margin = isWide ? "84px" : "64px";
 
   return (
-    <Box
-      maxWidth={width}
-    >
-    <Flex
-      as="nav"
-      align="center"
-      justify={["center", "center", "center", "space-between", "space-between"]}
-      wrap="wrap"
-      padding={["8px", "8px", "8px", "1rem", "1rem"]}
-      bg={background}
-      height={["170px", "170px", "170px", "78px", "78px"]}
-      boxShadow="md"
-    >
+    <Box maxWidth={width}>
       <Flex
+        as="nav"
         align="center"
-        direction="row"
-        display={["none", "none", "none", "block", "block"]}
+        justify={[
+          "center",
+          "center",
+          "center",
+          "space-between",
+          "space-between",
+        ]}
+        wrap="wrap"
+        padding={["8px", "8px", "8px", "1rem", "1rem"]}
+        bg={background}
+        height={["170px", "170px", "170px", "78px", "78px"]}
+        boxShadow="md"
       >
-        <Logo width="150px" marginLeft={margin} />
-      </Flex>
-
-      <Flex
-        align="center"
-        direction="row"
-        display={["none", "none", "none", "block", "block"]}
-        width={[0, 0, 0, 300, 400, 500]}
-      >
-        {searchInput && <SearchInput
-          placeholder="Where are you staying?"
-          ariaLabel="Search for homes based on location"
-          onSubmit={() => undefined}
-        />}
-      </Flex>
-
-      <Stack
-        spacing={3}
-        direction="row"
-        display={["none", "none", "none", "block", "block"]}
-        marginRight={margin}
-      >
-        <ButtonPrimary>Post Listing</ButtonPrimary>
-        <ButtonPrimary borderColor={buttonBorder}>Get Started</ButtonPrimary>
-      </Stack>
-
-      {/* Smaller screen size display */}
-      <Stack
-        spacing={2}
-        direction="column"
-        display={["block", "block", "block", "none", "none"]}
-      >
-        <Flex justify="center">
-          <Logo width="150px" background={background} />
+        <Flex
+          align="center"
+          direction="row"
+          display={["none", "none", "none", "block", "block"]}
+        >
+          <Logo width="150px" marginLeft={margin} />
         </Flex>
-        <Flex align="center">
-          {searchInput && <SearchInput
-            placeholder="Where are you staying?"
-            ariaLabel="Search for homes based on location"
-            onSubmit={() => undefined}
-          />}
+
+        <Flex
+          align="center"
+          direction="row"
+          display={["none", "none", "none", "block", "block"]}
+          width={[0, 0, 0, 300, 400, 500]}
+        >
+          {searchInput && (
+            <SearchInput
+              placeholder="Where are you staying?"
+              ariaLabel="Search for homes based on location"
+              onSubmit={() => undefined}
+            />
+          )}
         </Flex>
-        <Stack spacing={2} justify="center" direction="row">
+
+        <Stack
+          spacing={3}
+          direction="row"
+          display={["none", "none", "none", "block", "block"]}
+          marginRight={margin}
+        >
           <ButtonPrimary>Post Listing</ButtonPrimary>
           <ButtonPrimary borderColor={buttonBorder}>Get Started</ButtonPrimary>
         </Stack>
-      </Stack>
-    </Flex>
-  </Box>
+
+        {/* Smaller screen size display */}
+        <Stack
+          spacing={2}
+          direction="column"
+          display={["block", "block", "block", "none", "none"]}
+        >
+          <Flex justify="center">
+            <Logo width="150px" background={background} />
+          </Flex>
+          <Flex align="center">
+            {searchInput && (
+              <SearchInput
+                placeholder="Where are you staying?"
+                ariaLabel="Search for homes based on location"
+                onSubmit={() => undefined}
+              />
+            )}
+          </Flex>
+          <Stack spacing={2} justify="center" direction="row">
+            <ButtonPrimary>Post Listing</ButtonPrimary>
+            <ButtonPrimary borderColor={buttonBorder}>
+              Get Started
+            </ButtonPrimary>
+          </Stack>
+        </Stack>
+      </Flex>
+    </Box>
   );
 }
 
