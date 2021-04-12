4cee7d264f5c01f03efb62d752caea709114d129
Tommy Deng <git@tommydeng.com>
2021-04-12 00:52:41 -0400

    feat: tweak geocoder styles
    - also unsubscribe on unmount

--- components/core/SearchInput.tsx
+++ components/core/SearchInput.tsx
@@ -18,9 +18,11 @@ const geocoder = new MapboxGeocoder({
 });
 
 function SearchInput(props: SearchInputProps): ReactElement {
+  const { placeholder, ariaLabel, onSubmit } = props;
+
+  // @ts-ignore
   const [searchCoordinates, setSearchCoordinates] = useState();
 
-  const { placeholder, ariaLabel, onSubmit } = props;
   // const router = useRouter();
   // const [value, setValue] = useState("");
 
@@ -37,46 +39,51 @@ function SearchInput(props: SearchInputProps): ReactElement {
   // }, [router.query]);
 
   useEffect(() => {
-    geocoder.setPlaceholder(placeholder);
-  }, [placeholder]);
-
-  useEffect(() => {
-    geocoder.addTo("#location");
-    geocoder.on("result", ({ result }) => {
+    const handleResult = ({ result }: any) => {
       setSearchCoordinates(result);
-      console.log(searchCoordinates);
-    });
+      console.log(result);
+    };
+
+    geocoder.addTo("#location");
+    geocoder.setPlaceholder(placeholder);
+    geocoder.on("result", handleResult);
+
+    return () => {
+      geocoder.off("result", handleResult);
+    };
   }, []);
 
   return (
-    <>
-      <Flex
-        borderRadius="50px"
-        height="48px"
-        bg="white"
-        border="1px"
-        borderColor="common.dark"
-      >
-        <Box flex="1" id="location" w="100%" height="100%" />
+    <Flex
+      position="relative"
+      borderRadius="50px"
+      height="48px"
+      bg="white"
+      border="1px"
+      borderColor="common.dark"
+      align="center"
+    >
+      <Box flex="1" id="location" w="100%" height="100%" />
 
-        <IconButton
-          aria-label={ariaLabel}
-          margin="8px 8px 0 8px"
-          icon={<ArrowForwardIcon w={5} h={5} />}
-          isRound
-          backgroundColor="common.dark"
-          color="white"
-          size="sm"
-          _hover={{
-            backgroundColor: "brand.primary_hover",
-          }}
-          _active={{
-            backgroundColor: "brand.primary_active",
-          }}
-          onClick={onSubmit}
-        />
-      </Flex>
-    </>
+      <IconButton
+        position="absolute"
+        right={0}
+        aria-label={ariaLabel}
+        marginRight="10px"
+        icon={<ArrowForwardIcon w={5} h={5} />}
+        isRound
+        backgroundColor="common.dark"
+        color="white"
+        size="sm"
+        _hover={{
+          backgroundColor: "brand.primary_hover",
+        }}
+        _active={{
+          backgroundColor: "brand.primary_active",
+        }}
+        onClick={onSubmit}
+      />
+    </Flex>
   );
 }
 
