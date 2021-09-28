5f952594acb79f7da9590731ee2f5631f5dd6b80
Tommy Xu <txu045@uottawa.ca>
2021-09-27 20:07:45 -0400

    Removed redundent jest config in package.json and removed unnessesary comments and syntax

--- components/__tests__/sections/Header.spec.tsx
+++ components/__tests__/sections/Header.spec.tsx
@@ -1,20 +1,9 @@
 import React from "react";
 import { mount } from "enzyme";
 
-// Example test case to show valid setup of enzyme and jes
+// Example test case to show valid setup of enzyme and jest
 
 test("hello world", () => {
   const wrapper = mount(<p>Hello Jest!</p>);
   expect(wrapper.text()).toMatch("Hello Jest!");
 });
-
-// import React from "react";
-// import { shallow } from "enzyme";
-// import Header from "../../sections/Header";
-
-// describe("Header Test", () => {
-//   it("Button Exist", () => {
-//     const header = shallow(<Header darkTheme />);
-//     expect(header.exists("<ButtonPrimary>")).toBeTruthy();
-//   });
-// });
