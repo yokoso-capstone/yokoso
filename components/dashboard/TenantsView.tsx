e8cf1922113ac17a7850f3388477e9f4795f4051
Tommy Deng <git@tommydeng.com>
2021-12-02 23:00:54 -0500

    feat: add filtering for past/upcoming tenants

--- components/dashboard/TenantsView.tsx
+++ components/dashboard/TenantsView.tsx
@@ -1,4 +1,5 @@
-import { useMemo, useState, ReactElement } from "react";
+import { useMemo, useState, ReactElement, useEffect } from "react";
+import firebase from "firebase/app";
 import { ButtonSecondary } from "@/components/core/Button";
 import { DashboardCard } from "@/components/core/Layout";
 import { TabPrimary } from "@/components/core/Tabs";
@@ -18,50 +19,134 @@ import {
   Th,
   Td,
 } from "@chakra-ui/react";
-import { TenantEntry } from "@/src/api/types";
+import { RequestStatus, TenantRequest, UserPublic } from "@/src/api/types";
 import { auth } from "@/src/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { usersPrivate, CollectionName } from "@/src/api/collections";
+import { tenantRequests, usersPublic } from "@/src/api/collections";
 import { useCollectionOnce } from "react-firebase-hooks/firestore";
 
 enum TimePeriod {
   Past = "past",
-  Current = "current",
   Upcoming = "upcoming",
 }
 
-function TenantsView(): ReactElement {
-  // @ts-ignore
-  const [selectedPeriod, setSelectedPeriod] = useState<TimePeriod>(
-    TimePeriod.Past
-  );
+type MergedTenantData = {
+  tenantUid: string;
+  userPublic?: UserPublic;
+  tenantRequests: TenantRequest[];
+};
 
+type ReducedTenantData = MergedTenantData[];
+
+function TenantsView(): ReactElement {
+  const [timePeriod, setTimePeriod] = useState(TimePeriod.Past);
   const [user] = useAuthState(auth);
-  // TODO: query based on time period
-  const query = useMemo(
-    () =>
-      user
-        ? usersPrivate.doc(user.uid).collection(CollectionName.Tenants)
-        : undefined,
-    [user]
+  const query = useMemo(() => {
+    const acceptedStatus: RequestStatus = "accepted";
+
+    return user
+      ? tenantRequests
+          .where("landlordUid", "==", user.uid)
+          .where("status", "==", acceptedStatus)
+      : undefined;
+  }, [user]);
+  const [reducedTenantData, setReducedTenantData] = useState<ReducedTenantData>(
+    []
   );
   const [snapshot] = useCollectionOnce(query);
-  const tenants = snapshot?.docs.map(
-    (doc) =>
-      (({
-        ...doc.data(),
-        id: doc.id,
-      } as unknown) as TenantEntry)
-  );
+
+  useEffect(() => {
+    // TODO: avoid messy client-side restructuring data and handle in separate collection managed by Stripe webhook
+    const handleMerge = async () => {
+      const acceptedTenantRequests = snapshot?.docs.map(
+        (doc) =>
+          (({
+            ...doc.data(),
+            id: doc.id,
+          } as unknown) as TenantRequest)
+      );
+      const mergedRequestAndUserData = acceptedTenantRequests?.map(
+        async (request) => {
+          const { tenantUid } = request;
+          const userRef = usersPublic.doc(tenantUid);
+          const userSnapshot = await userRef.get();
+          const userData = userSnapshot.data() as UserPublic;
+
+          return {
+            tenantUid,
+            userPublic: userData,
+            tenantRequest: request,
+          };
+        }
+      );
+
+      if (mergedRequestAndUserData) {
+        const mergedData = await Promise.all(mergedRequestAndUserData);
+        const collectUnique: { [tenantUid: string]: MergedTenantData } = {};
+
+        mergedData.forEach((data) => {
+          const { tenantUid, tenantRequest, userPublic } = data;
+
+          if (Object.prototype.hasOwnProperty.call(collectUnique, tenantUid)) {
+            collectUnique[tenantUid].tenantRequests.push(tenantRequest);
+          } else {
+            collectUnique[tenantUid] = {
+              tenantUid,
+              userPublic,
+              tenantRequests: [tenantRequest],
+            };
+          }
+        });
+
+        setReducedTenantData(Object.values(collectUnique));
+      }
+    };
+
+    handleMerge();
+  }, [snapshot]);
+
+  const filteredTenantData = reducedTenantData.filter((tenantData) => {
+    const { tenantRequests } = tenantData;
+
+    const requestToTimestampMillis = (request: TenantRequest) => {
+      // TODO: handle sketchy fallback onto leave availability better (avoids errors on old data)
+      const leaseStartDate =
+        (request.leaseStartDate as firebase.firestore.Timestamp) ??
+        firebase.firestore.Timestamp.fromDate(
+          new Date(
+            (request.listing.data.lease.availability as unknown) as string
+          )
+        );
+      const timestampMillis = leaseStartDate.toMillis();
+
+      return timestampMillis;
+    };
+
+    const hasPastListing = tenantRequests.some((request) => {
+      const timestampMillis = requestToTimestampMillis(request);
+      const now = Date.now();
+
+      return timestampMillis <= now;
+    });
+
+    const hasUpcomingListing = tenantRequests.some((request) => {
+      const timestampMillis = requestToTimestampMillis(request);
+      const now = Date.now();
+
+      return timestampMillis > now;
+    });
+
+    return timePeriod === TimePeriod.Past ? hasPastListing : hasUpcomingListing;
+  });
 
   return (
     <DashboardCard>
       <Tabs isLazy>
         <TabList>
-          <TabPrimary onClick={() => setSelectedPeriod(TimePeriod.Past)}>
+          <TabPrimary onClick={() => setTimePeriod(TimePeriod.Past)}>
             Past
           </TabPrimary>
-          <TabPrimary onClick={() => setSelectedPeriod(TimePeriod.Upcoming)}>
+          <TabPrimary onClick={() => setTimePeriod(TimePeriod.Upcoming)}>
             Upcoming
           </TabPrimary>
           <Spacer />
@@ -72,13 +157,10 @@ function TenantsView(): ReactElement {
 
         <TabPanels>
           <TabPanel>
-            <TenantTable tenants={tenants} />
+            <TenantTable tenantData={filteredTenantData} />
           </TabPanel>
           <TabPanel>
-            <TenantTable tenants={tenants} />
-          </TabPanel>
-          <TabPanel>
-            <TenantTable tenants={tenants} />
+            <TenantTable tenantData={filteredTenantData} />
           </TabPanel>
         </TabPanels>
       </Tabs>
@@ -86,8 +168,8 @@ function TenantsView(): ReactElement {
   );
 }
 
-const TenantTable = (props: { tenants?: TenantEntry[] }) => {
-  const { tenants } = props;
+const TenantTable = (props: { tenantData: ReducedTenantData }) => {
+  const { tenantData } = props;
 
   return (
     <Table>
@@ -100,18 +182,18 @@ const TenantTable = (props: { tenants?: TenantEntry[] }) => {
         </Tr>
       </Thead>
       <Tbody>
-        {tenants?.map((tenant, index) => (
+        {tenantData.map((data, index) => (
           <Tr key={index}>
             <Td>
               <Image
-                src={tenant.profilePicture}
+                src={data.userPublic?.profilePicture}
                 boxSize="64px"
                 objectFit="cover"
                 borderRadius="full"
               />
             </Td>
-            <Td>{tenant.firstName}</Td>
-            <Td>{tenant.lastName}</Td>
+            <Td>{data.userPublic?.firstName}</Td>
+            <Td>{data.userPublic?.lastName}</Td>
             <Td>
               <ButtonSecondary>View Details</ButtonSecondary>
             </Td>
